9b77e65c58348f3a4de515ef9eb74e3f2f7d4a3f
Your Name <you@example.com>
2017-05-08 11:23:15 +0200

    Can now handle REST and WS together

--- backend/src/main/java/foo/bar/App.java
+++ backend/src/main/java/foo/bar/App.java
@@ -1,6 +1,7 @@
 package foo.bar;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -9,13 +10,26 @@ import org.glassfish.jersey.servlet.ServletContainer;
 public class App {
 
     public static void main(String [] args) throws Exception {
+        Server server = new Server();
+        //Listen on 2222
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(2222);
+        server.addConnector(connector);
+
+        // Setup the basic application "context" for this application at "/"
+        // This is also known as the handler tree (in jetty speak)
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
         ResourceConfig config = new ResourceConfig();
         config.packages("foo.bar");
-        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+        ServletHolder servlet = new ServletHolder("rs-rest", new ServletContainer(config));
+        context.addServlet(servlet, "/rest/*");
 
-        Server server = new Server(2222);
-        ServletContextHandler context = new ServletContextHandler(server, "/*");
-        context.addServlet(servlet, "/*");
+        // Add a websocket to a specific path spec
+        ServletHolder holderEvents = new ServletHolder("ws-events", EventServlet.class);
+        context.addServlet(holderEvents, "/events/*");
 
         try {
             server.start();
