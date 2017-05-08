ebb821e2878cf0c31980dd5277dedf647915dac6
Your Name <you@example.com>
2017-05-08 14:12:32 +0200

    Connected clients are now notified upon Pixel change

--- backend/src/main/java/foo/bar/board/Board.java
+++ backend/src/main/java/foo/bar/board/Board.java
@@ -1,8 +1,14 @@
 package foo.bar.board;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import foo.bar.websocket.EventSocket;
+import foo.bar.websocket.PooledSessionCreator;
+
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Board {
 
@@ -57,10 +63,26 @@ public class Board {
         // Change actual Pixel color
         int index = toSet.getX() + toSet.getY() * xMaximum;
         pixels[index] = toSet;
+
+        // Update all connected clients
+        Set<EventSocket> websockets = PooledSessionCreator.getWebsockets();
+        String toSetStr = serialize(toSet);
+        websockets.parallelStream().forEach(eventSocket -> eventSocket.sendMessage(toSetStr));
     }
 
     private boolean isAllowed(String user) {
         return userChanges.get(user) == null ||
                 userChanges.get(user).isBefore(LocalDateTime.now().minusMinutes(5));
+    }
+
+    private String serialize(Pixel toSet) {
+        ObjectMapper mapper = new ObjectMapper();
+        String toSetStr;
+        try {
+            toSetStr = mapper.writeValueAsString(toSet);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error wile converting Pixel to String.", e);
+        }
+        return toSetStr;
     }
 }
