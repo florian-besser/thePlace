4d4f317293169b6cd7ef4092dbfde1b1d6151b61
Dominic Langenegger <dola@gmx.ch>
2017-05-10 09:24:13 +0200

    add history logger to store sequence of pixels and colors set

---
+++ model/src/main/java/history/HistoryLogger.java
@@ -0,0 +1,13 @@
+package history;
+
+import foo.bar.model.SimpleColor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HistoryLogger {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HistoryLogger.class);
+
+    public static void logPixelSet(Integer x, Integer y, SimpleColor color) {
+        LOGGER.info(String.format("%d/%d;%s", x, y, color.getColor()));
+    }
+}
