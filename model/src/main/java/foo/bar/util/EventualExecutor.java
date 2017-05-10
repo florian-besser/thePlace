7a145b910a5726736c77e237d1a5376a3da0bd83
Your Name <you@example.com>
2017-05-10 09:45:12 +0200

    Better log output

--- model/src/main/java/foo/bar/util/EventualExecutor.java
+++ model/src/main/java/foo/bar/util/EventualExecutor.java
@@ -1,11 +1,14 @@
 package foo.bar.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class EventualExecutor<T, R> {
-
-    public static final int MAX_TRIES = 10;
+    public static final int MAX_TRIES = 30;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventualExecutor.class);
 
     public R tryExecute(Function<T, R> func, T argument) {
         int count = 0;
@@ -13,7 +16,9 @@ public class EventualExecutor<T, R> {
             try {
                 return func.apply(argument);
             } catch (Exception e) {
+                LOGGER.warn("Failed to execute function");
                 if (++count == MAX_TRIES) {
+                    LOGGER.error("Failed to execute function", e);
                     throw e;
                 } else {
                     try {
@@ -33,7 +38,9 @@ public class EventualExecutor<T, R> {
                 func.accept(argument);
                 return;
             } catch (Exception e) {
+                LOGGER.warn("Failed to execute consumer");
                 if (++count == MAX_TRIES) {
+                    LOGGER.error("Failed to execute consumer", e);
                     throw e;
                 } else {
                     try {
